7665671c5c83299c2d3041c9210c9b9082cc9587
sbx_user1059 <sbx_user1059@ip-10-128-115-228.eu-west-2.compute.internal>
2019-02-10 11:30:33 +0000

    Sun Feb 10 11:30:33 UTC 2019

--- src/main/java/befaster/solutions/HLO/HelloSolution.java
+++ src/main/java/befaster/solutions/HLO/HelloSolution.java
@@ -3,9 +3,20 @@ package befaster.solutions.HLO;
 import befaster.runner.SolutionNotImplementedException;
 
 public class HelloSolution {
-    public String hello(String friendName) {
-       return "Hello, World!";
-    }
+	public String hello(String friendName) {
+
+		if(friendName !=null){
+			if(friendName.length() ==0){
+				//can we change the system to ask this question?
+				return "Hello, i do not know your name.";    	  
+			}else{
+				return "Hello, " + friendName + "!";
+			}
+		}
+		//should we return nothing? should we return a blank?
+		return "Hello, i do not know your name.";    
+	}
 }
+
 
 
