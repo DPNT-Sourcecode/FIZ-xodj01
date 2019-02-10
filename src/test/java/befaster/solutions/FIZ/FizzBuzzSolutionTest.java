1ad86f644575f798ebaf74e5dc2cd45153f37671
sbx_user1059 <sbx_user1059@ip-10-128-115-228.eu-west-2.compute.internal>
2019-02-10 11:39:51 +0000

    Sun Feb 10 11:39:51 UTC 2019

--- src/test/java/befaster/solutions/FIZ/FizzBuzzSolutionTest.java
+++ src/test/java/befaster/solutions/FIZ/FizzBuzzSolutionTest.java
@@ -3,6 +3,9 @@ package befaster.solutions.FIZ;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
 public class FizzBuzzSolutionTest {
 
 	private FizzBuzzSolution solution;
@@ -14,15 +17,21 @@ public class FizzBuzzSolutionTest {
 	
 	@Test
 	public void testFizz(){
-		
-		
+	 assertThat(solution.fizzBuzz(3), equalTo("fizz"));
 	}
 	@Test
+	public void testFizzDivisible(){
+	 //could potentially do this with an input file of multiple values divisible..
+	 assertThat(solution.fizzBuzz(9), equalTo("fizz"));
+	}
+	
+	@Test
 	public void testBuzz(){
-		
+		 assertThat(solution.fizzBuzz(5), equalTo("buzz"));
 	}
 	@Test
 	public void testFizzBuzz(){
 		
 	}
 }
+
